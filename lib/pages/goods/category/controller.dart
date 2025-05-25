a8242d6b47bcbeb417f0b9cbdb6873c0b1562e98
DHLau <ldhtrythebest@163.com>
2025-05-25 15:46:59 +0900

    add category Controller logic

--- lib/pages/goods/category/controller.dart
+++ lib/pages/goods/category/controller.dart
@@ -1,13 +1,120 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:woo_shop/common/index.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class CategoryController extends GetxController {
   CategoryController();
 
-  _initData() {
+  // 分类ID
+  int categoryId = Get.arguments['id'];
+
+  // 分类数据
+  List<CategoryModel> categoryItems = [];
+
+  _initData() async {
+    // 读取缓存
+    var stringCategories =
+        Storage().getString(Constants.storageProductCategories);
+    categoryItems = stringCategories != ""
+        ? jsonDecode(stringCategories).map<CategoryModel>((item) {
+            return CategoryModel.fromJson(item);
+          }).tolist()
+        : [];
+
+    // 如果缓存为空, 从网络后驱
+    if (categoryItems.isEmpty) {
+      // 获取分类数据
+      categoryItems = await ProductApi.categories();
+    }
     update(["category"]);
   }
 
   void onTap() {}
+
+  // 分类点击事件
+  void onCategoryTap(int id) async {
+    categoryId = id;
+    // 刷新数据
+    refreshController.requestRefresh();
+  }
+
+  // 列表
+  List<ProductModel> items = [];
+
+  // 页码
+  int _page = 1;
+
+  // 页尺寸
+  final int _limit = 20;
+
+  final RefreshController refreshController = RefreshController(
+    initialRefresh: true, // 一开始就会自动下拉刷新
+  );
+
+  // 加载数据
+  // 拉数据
+  Future<bool> _loadSearch(bool isRefresh) async {
+    // 拉取数据
+    var result = await ProductApi.products(ProductsReq(
+      // 刷新,重置页数1
+      page: isRefresh ? 1 : _page,
+      prePage: _limit,
+      category: categoryId.toString(),
+    ));
+
+    // 下拉刷新
+    if (isRefresh) {
+      _page = 1;
+      items.clear(); // 清空数据
+    }
+
+    // 有数据
+    if (result.isNotEmpty) {
+      // 页数+1
+      _page += 1;
+      items.addAll(result);
+    }
+    // 是否是空
+    return result.isEmpty;
+  }
+
+  // 上拉载入新商品
+  void onLoading() async {
+    if (items.isNotEmpty) {
+      try {
+        // 拉取数据
+        var isEmpty = await _loadSearch(false);
+
+        if (isEmpty) {
+          // 设置无数据
+          refreshController.loadNoData();
+        } else {
+          // 刷新完成
+          refreshController.loadComplete();
+        }
+      } catch (e) {
+        refreshController.loadFailed();
+      }
+    } else {
+      // 设置无数据
+      refreshController.loadNoData();
+    }
+    update(["product_list"]);
+  }
+
+  // 下拉刷新
+  void onRefresh() async {
+    try {
+      await _loadSearch(true);
+      refreshController.refreshCompleted();
+    } catch (error) {
+      // 刷新失败
+      refreshController.refreshFailed();
+    }
+    update(["product_list"]);
+  }
 
   // @override
   // void onInit() {
@@ -20,8 +127,9 @@ class CategoryController extends GetxController {
     _initData();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  @override
+  void onClose() {
+    super.onClose();
+    refreshController.dispose();
+  }
 }
